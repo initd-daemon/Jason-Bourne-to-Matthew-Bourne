a6ed4618a6e3622c8b9e26db6e43f1a9d99ea612
Max Campbell <maxc@maxc.in>
2016-10-05 22:14:30 +0100

    changed from cloud to matt

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,24 +1,24 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
-	
+
 	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
 	    || node.classList.indexOf('ace_editor') > -1) {
 		return;
 	}
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -32,16 +32,14 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bJason Bourne\b/g, "Matthew Bourne the networking specialist");
+	v = v.replace(/\bJason bourne\b/g, "Matthew bourne the networking specialist");
+	v = v.replace(/\bjason Bourne\b/g, "matthew Bourne the networking specialist");
+	v = v.replace(/\bjason bourne\b/g, "matthew bourne the networking specialist");
+
 	textNode.nodeValue = v;
 }
-
-
